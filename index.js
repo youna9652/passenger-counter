9e6380103f5be0363491322ea27db50302b55000
youna9652 <youna.jung@mail.utoronto.ca>
2021-08-27 23:27:47 -0400

    finished the app

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 let count = 0;
+let countStr = 0;
 
 function increment() {
-  count = count + 1;
+  count += 1;
   countEl.innerText = count;
 }
 
 function save() {
   console.log(count);
+  countStr = count + " - ";
+  saveEl.textContent += countStr;
+  countEl.innerText = 0;
+  count = 0;
 }
